dc9248800ae83d49a7f638a0331ebeffb2621f59
Ivan <ivan.kranjec95@gmail.com>
2022-06-21 22:09:28 +0200

    [Contact us page] - add contact us page-

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,10 @@
+import { Button } from "rsuite"
+import { Page } from "../../UI/Page/Page"
+
+export const Contact = () => {
+    return (
+        <Page header={'Contact us'}>
+            <Button children={'Click here to contact us!'} />
+        </Page>
+    )
+}

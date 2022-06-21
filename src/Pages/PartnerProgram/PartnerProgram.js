b3824147cf9ce4b1532ea0824db2c340af67dee7
Ivan <ivan.kranjec95@gmail.com>
2022-06-21 22:10:01 +0200

    [Partner program page] - add partner program page.

---
+++ src/Pages/PartnerProgram/PartnerProgram.js
@@ -0,0 +1,10 @@
+import { Button } from "rsuite"
+import { Page } from "../../UI/Page/Page"
+
+export const PartnerProgram = () => {
+    return (
+        <Page header={'Become our partner'}>
+            <Button children={'Click here to become partner!'} />
+        </Page>
+    )
+}
